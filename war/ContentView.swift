39f9b3fc0f640534fce46ec5100ae826702e38e5
Sean <vaezsean@gmail.com>
2021-07-21 15:30:33 -0400

    initial commit

--- war/ContentView.swift
+++ war/ContentView.swift
@@ -9,13 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+ZStack{
+    
+    Image("background")
+    .ignoresSafeArea()
+    
+            
+        VStack{ Spacer()
+            Image("logo")
+            Spacer()
+        
+            HStack{
+                Spacer()
+                Image("card3")
+            Spacer()
+            
+            Image("card4")
+                Spacer()
+            }
+            Spacer()
+            
+            Image("dealbutton")
+            Spacer()
+            
+            HStack{
+                Spacer()
+                VStack{
+                    Text("Player 1")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                    Text("0")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                        }
+                Spacer()
+                VStack{
+                    Text("Player 2")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                    Text("0")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                
+
+                }
+                Spacer()
+            }
+            Spacer()
+        }
     }
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+
+
+}
+
 }
